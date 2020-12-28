175ebe20ca26cbf87005a205e793fb9381fce6f5
Kelvin Mai <kelvin.mai@bonotel.com>
2020-12-28 12:38:51 -0800

    initial commit
    - initial commit from create-shadow-cljs-app
    - move cljs into src/cljs
    - created deps.edn for backend
    - created src/clj

---
+++ src/cljs/browser/main.cljs
@@ -0,0 +1,7 @@
+(ns browser.main)
+
+(println "Hello, world! - this is from the module static code - Open your Console in DevTools")
+
+(defn init []
+  (println "This is from the init function")
+  (.appendChild (.-body js/document) (.createTextNode js/document "It works!")))
