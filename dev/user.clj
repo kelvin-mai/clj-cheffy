c4b1e3b055cd614c21a445cb39c75bdbe78a3235
Kelvin Mai <kelvin.mai@bonotel.com>
2020-12-28 13:55:56 -0800

    added integrant

--- dev/user.clj
+++ dev/user.clj
@@ -1,1 +1,18 @@
-(ns user)
+(ns user
+  (:require [cheffy.core]
+            [integrant.core :as ig]
+            [integrant.repl :as ig-repl :refer [go
+                                                halt
+                                                reset
+                                                reset-all]]))
+
+(ig-repl/set-prep!
+  (fn [] (-> "resources/config.edn"
+             slurp
+             ig/read-string)))
+
+(comment
+  (go)
+  (halt)
+  (reset)
+  (reset-all))
--- src/clj/cheffy/core.clj
+++ src/clj/cheffy/core.clj
@@ -1,1 +1,37 @@
-(ns cheffy.core)
+(ns cheffy.core
+  (:require [integrant.core :as ig]
+            [reitit.ring :as ring]
+            [ring.adapter.jetty :as jetty]))
+
+(defn app [env]
+  (ring/ring-handler
+    (ring/router
+      [["/" {:get (fn [_]
+                    {:status 200
+                     :body "hello world"})}]])))
+
+(defmethod ig/init-key :server/jetty
+  [_ {:keys [handler port]}]
+  (println (str "Server running on port " port))
+  (jetty/run-jetty handler {:port port
+                            :join? false}))
+
+(defmethod ig/init-key :cheffy/app
+  [_ config]
+  (app config))
+
+(defmethod ig/init-key :db/postgres
+  [_ config]
+  (println "Configured database")
+  (:jdbc-url config))
+
+(defmethod ig/halt-key! :server/jetty
+  [_ jetty]
+  (.stop jetty))
+
+(defn -main
+  [config-file]
+  (let [config (-> config-file
+                   slurp
+                   ig/read-string)]
+    (ig/init config)))
